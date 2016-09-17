16ff336a0f94345ec32e738e609c46e905536e35
marthematicist <hite.32@gmail.com>
2016-09-17 11:40:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setupGlobalVariables() {
   drawColor = color( 0 , 0 , 0 , 1);
   markerColor = color( 0 , 0 , 0 , 1);
   // how transparent the marker is
-  markerAlpha = 0.05;
+  markerAlpha = 0.1;
   // number of frames to wait while initializing
   waitFrames = 200;
   // frame counter;
